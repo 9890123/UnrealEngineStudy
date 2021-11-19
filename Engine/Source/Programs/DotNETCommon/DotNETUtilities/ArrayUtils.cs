642b30401cb353dc96c28230f81b50af1c2e37c8
lijiejun <9890123@qq.com>
2021-11-19 17:29:40 +0800

    2021/11/19

---
+++ Engine/Source/Programs/DotNETCommon/DotNETUtilities/ArrayUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tools.DotNETCommon
+{
+    static class ArrayUtils
+    {
+        public static bool ByteArraysEqual(byte[] A, byte[] B)
+        {
+            if (A.Length != B.Length)
+            {
+                return false;
+            }
+
+            for (int Idx = 0; Idx < A.Length; Idx++)
+            {
+                if (A[Idx] != B[Idx])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
