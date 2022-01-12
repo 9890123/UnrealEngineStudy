b1bc47ed6b5bcdfabfd1a294dc7e477595fea42f
lijiejun <9890123@qq.com>
2022-01-12 11:11:33 +0800

    2022/01/12

---
+++ Engine/Source/Programs/DotNETCommon/DotNETUtilities/BinaryArchiveReader.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.DotNETCommon
+{
+    public class BinaryArchiveReader : IDisposable
+    {
+        Stream Stream;
+        byte[] Buffer;
+        int BufferPos;
+        List<object> Objects = new List<object>();
+
+        public BinaryArchiveReader(byte[] Buffer)
+        {
+            this.Buffer = Buffer;
+        }
+
+        public BinaryArchiveReader(FileReference FileName)
+        {
+            this.Buffer = FileReference.ReadAllBytes(FileName);
+        }
+
+        public BinaryArchiveReader(Stream Stream)
+        {
+            Buffer = new byte[Stream.Length];
+            Stream.Read(Buffer, 0, Buffer.Length);
+        }
+
+        public void Dispose()
+        {
+            if (Stream != null)
+            {
+                Stream.Dispose();
+                Stream = null;
+            }
+
+            Buffer = null;
+        }
+
+        public bool ReadBool()
+        {
+            return ReadByte() != 0;
+        }
+
+        public byte ReadByte()
+        {
+            byte Value = Buffer[BufferPos];
+            BufferPos++;
+            return Value;
+        }
+
+        public sbyte ReadSignedByte()
+        {
+            return (sbyte)ReadByte();
+        }
+
+        public short ReadShort()
+        {
+            return (short)ReadUnsignedShort();
+        }
+
+        public ushort ReadUnsignedShort()
+        {
+            ushort Value = (ushort)(Buffer[BufferPos + 0] | (Buffer[BufferPos + 1] << 8));
+            BufferPos += 2;
+            return Value;
+        }
+
+        public int ReadInt()
+        {
+            return (int)ReadUnsignedInt();
+        }
+
+        public uint ReadUnsignedInt()
+        {
+            uint Value = (uint)(Buffer[BufferPos + 0] | (Buffer[BufferPos + 1] << 8) | (Buffer[BufferPos + 2] << 16) | (Buffer[BufferPos + 3] << 24));
+            BufferPos += 4;
+            return Value;
+        }
+
+        public long ReadLong()
+        {
+            return (long)ReadUnsignedLong();
+        }
+
+        public ulong ReadUnsignedLong()
+        {
+            ulong Value = (ulong)ReadUnsignedInt();
+            Value |= (ulong)ReadUnsignedInt() << 32;
+            return Value;
+        }
+
+        public string ReadString()
+        {
+            byte[] Bytes = ReadByteArray();
+            if (Bytes == null)
+            {
+                return null;
+            }
+            else
+            {
+                return Encoding.UTF8.GetString(Bytes);
+            }
+        }
+
+        public byte[] ReadByteArray()
+        {
+            return ReadPrimitiveArray<byte>(sizeof(byte));
+        }
+
+        public short[] ReadShortArray()
+        {
+            return ReadPrimitiveArray<short>(sizeof(short));
+        }
+
+        public int[] ReadIntArray()
+        {
+            return ReadPrimitiveArray<int>(sizeof(int));
+        }
+
+        private T[] ReadPrimitiveArray<T>(int ElementSize) where T : struct
+        {
+            int Length = ReadInt();
+            if (Length < 0)
+            {
+                return null;
+            }
+            else
+            {
+                T[] Result = new T[Length];
+                ReadBulkData(Result, Length * ElementSize);
+                return Result;
+            }
+        }
+
+        public byte[] ReadFixedSizeByteArray(int Length)
+        {
+            return ReadFixedSizePrimitiveArray<byte>(sizeof(byte), Length);
+        }
+
+        public short[] ReadFixedSizeShortArray(int Length)
+        {
+            return ReadFixedSizePrimitiveArray<short>(sizeof(short), Length);
+        }
+
+        public int[] ReadFixedSizeIntArray(int Length)
+        {
+            return ReadFixedSizePrimitiveArray<int>(sizeof(int), Length);
+        }
+
+        private T[] ReadFixedSizePrimitiveArray<T>(int ElementSize, int ElementCount) where T : struct
+        {
+            T[] Result = new T[ElementCount];
+            ReadBulkData(Result, ElementSize * ElementCount);
+            return Result;
+        }
+
+        private void ReadBulkData(Array Data, int Size)
+        {
+            System.Buffer.BlockCopy(Buffer, BufferPos, Data, 0, Size);
+            BufferPos += Size;
+        }
+
+        public T[] ReadArray<T>(Func<T> ReadElement)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }
+            else
+            {
+                T[] Result = new T[Count];
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result[Idx] = ReadElement();
+                }
+                return Result;
+            }
+        }
+
+        public List<T> ReadList<T>(Func<T> ReadElement)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }
+            else
+            {
+                List<T> Result = new List<T>(Count);
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result.Add(ReadElement());
+                }
+                return Result;
+            }
+        }
+
+        public HashSet<T> ReadHashSet<T>(Func<T> ReadElement)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }
+            else
+            {
+                HashSet<T> Result = new HashSet<T>();
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result.Add(ReadElement());
+                }
+                return Result;
+            }
+        }
+
+        public HashSet<T> ReadHashSet<T>(Func<T> ReadElement, IEqualityComparer<T> Comparer)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }
+            else
+            {
+                HashSet<T> Result = new HashSet<T>(Comparer);
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result.Add(ReadElement());
+                }
+                return Result;
+            }
+        }
+
+        public Dictionary<K, V> ReadDictionary<K, V>(Func<K> ReadKey, Func<V> ReadValue)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }else
+            {
+                Dictionary<K, V> Result = new Dictionary<K, V>(Count);
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result.Add(ReadKey(), ReadValue());
+                }
+                return Result;
+            }
+        }
+
+        public Dictionary<K, V> ReadDictionary<K, V>(Func<K> ReadKey, Func<V> ReadValue, IEqualityComparer<K> Comparer)
+        {
+            int Count = ReadInt();
+            if (Count < 0)
+            {
+                return null;
+            }
+            else
+            {
+                Dictionary<K, V> Result = new Dictionary<K, V>(Count, Comparer);
+                for (int Idx = 0; Idx < Count; Idx++)
+                {
+                    Result.Add(ReadKey(), ReadValue());
+                }
+                return Result;
+            }
+        }
+
+        public Nullable<T> ReadNullable<T>(Func<T> ReadValue) where T : struct
+        {
+            if (ReadBool())
+            {
+                return new Nullable<T>(ReadValue());
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public T ReadOptionalObject<T>(Func<T> Read) where T : class
+        {
+            if (ReadBool())
+            {
+                return Read();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public T ReadObjectReference<T>(Func<T> CreateObject, Action<T> ReadObject) where T : class
+        {
+            int Index = ReadInt();
+            if (Index < 0)
+            {
+                return null;
+            }
+            else
+            {
+                if (Index == Objects.Count)
+                {
+                    T Object = CreateObject();
+                    Objects.Add(Object);
+                    ReadObject(Object);
+                }
+                return (T)Objects[Index];
+            }
+        }
+
+        public T ReadObjectReference<T>(Func<T> ReadObject) where T : class
+        {
+            int Index = ReadInt();
+            if (Index < 0)
+            {
+                return null;
+            }
+            else
+            {
+                if (Index == Objects.Count)
+                {
+                    Objects.Add(null);
+                    Objects[Index] = ReadObject();
+                }
+                if (Objects[Index] == null)
+                {
+                    throw new InvalidOperationException("Attempt to serialize reference to object recursively.");
+                }
+                return (T)Objects[Index];
+            }
+        }
+    }
+}
---
+++ Engine/Source/Programs/DotNETCommon/DotNETUtilities/FileSystemReference.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.DotNETCommon
+{
+    [Serializable]
+    public abstract class FileSystemReference
+    {
+        public readonly string FullName;
+
+        public static readonly StringComparer Comparer = StringComparer.OrdinalIgnoreCase;
+
+        public static readonly StringComparison Comparison = StringComparison.OrdinalIgnoreCase;
+
+        protected FileSystemReference(string InFullName)
+        {
+            FullName = InFullName;
+        }
+
+        protected FileSystemReference(string InFullName, string InCanonicalName)
+        {
+            FullName = InFullName;
+        }
+
+        static protected string CombineStrings(DirectoryReference BaseDirectory, params string[] Fragments)
+        {
+            StringBuilder NewFullName = new StringBuilder(BaseDirectory.FullName);
+            if (NewFullName.Length > 0 && NewFullName[NewFullName.Length - 1] == Path.DirectorySeparatorChar)
+            {
+                NewFullName.Remove(NewFullName.Length - 1, 1);
+            }
+
+            foreach (string Fragment in Fragments)
+            {
+                if ((Fragment.Length >= 2 && Fragment[1] == ':') || (Fragment.Length >= 1 && (Fragment[0] == '\\' || Fragment[0] == '/')))
+                {
+                    NewFullName.Clear();
+                    NewFullName.Append(Path.GetFullPath(Fragment).TrimEnd(Path.DirectorySeparatorChar));
+                }
+                else
+                {
+                    int StartIdx = 0;
+                    while (StartIdx < Fragment.Length)
+                    {
+                        int EndIdx = StartIdx;
+                        while (EndIdx < Fragment.Length && Fragment[EndIdx] != '\\' && Fragment[EndIdx] != '/')
+                        {
+                            EndIdx++;
+                        }
+
+                        int Length = EndIdx - StartIdx;
+                        if (Length == 0)
+                        {
+                            throw new ArgumentException(String.Format("Path fragment '{0}' contains invalid directory separators.", Fragment));
+                        }
+                        else if (Length == 2 && Fragment[StartIdx] == '.' && Fragment[StartIdx + 1] == '.')
+                        {
+                            for (int SeparatorIdx = NewFullName.Length - 1; SeparatorIdx >= 0; SeparatorIdx--)
+                            {
+                                if (NewFullName[SeparatorIdx] == Path.DirectorySeparatorChar)
+                                {
+                                    NewFullName.Remove(SeparatorIdx, NewFullName.Length - SeparatorIdx);
+                                    break;
+                                }
+                            }
+                        }
+                        else if (Length != 1 || Fragment[StartIdx] != '.')
+                        {
+                            NewFullName.Append(Path.DirectorySeparatorChar);
+                            NewFullName.Append(Fragment, StartIdx, Length);
+                        }
+
+                        StartIdx = EndIdx + 1;
+                    }
+                }
+            }
+
+            if (NewFullName.Length == 0 || (NewFullName.Length == 2 && NewFullName[1] == ':'))
+            {
+                NewFullName.Append(Path.DirectorySeparatorChar);
+            }
+
+            return NewFullName.ToString();
+        }
+
+        public bool HasExtension(string Extension)
+        {
+            if (Extension.Length > 0 && Extension[0] != '.')
+            {
+                return FullName.Length >= Extension.Length + 1 && FullName[FullName.Length - Extension.Length - 1] == '.' && FullName.EndsWith(Extension, Comparison);
+            }
+            else
+            {
+                return FullName.EndsWith(Extension, Comparison);
+            }
+        }
+
+        public bool IsUnderDirectory(DirectoryReference Other)
+        {
+            return FullName.StartsWith(Other.FullName, Comparison) && (FullName.Length == Other.FullName.Length || FullName[Other.FullName.Length] == Path.DirectorySeparatorChar || Other.IsRootDirectory());
+        }
+
+        public bool ContainsName(string Name, int Offset)
+        {
+            return ContainsName(Name, Offset, FullName.Length - Offset);
+        }
+
+        public bool ContainsName(string Name, int Offset, int Length)
+        {
+            if (Length < Name.Length)
+            {
+                return false;
+            }
+
+            int MatchIdx = Offset;
+            for (; ; )
+            {
+                MatchIdx = FullName.IndexOf(Name, MatchIdx, Offset + Length - MatchIdx, Comparison);
+                if (MatchIdx == -1)
+                {
+                    return false;
+                }
+
+                int MatchEndIdx = MatchIdx + Name.Length;
+                if (FullName[MatchIdx - 1] == Path.DirectorySeparatorChar && (MatchEndIdx == FullName.Length || FullName[MatchEndIdx] == Path.DirectorySeparatorChar))
+                {
+                    return true;
+                }
+
+                MatchIdx += Name.Length;
+            }
+        }
+
+        public bool ContainsName(string Name, DirectoryReference BaseDir)
+        {
+            if (!IsUnderDirectory(BaseDir))
+            {
+                return false;
+            }
+            else
+            {
+                return ContainsName(Name, BaseDir.FullName.Length);
+            }
+        }
+
+        public bool ContainsAnyNames(IEnumerable<string> Names, DirectoryReference BaseDir)
+        {
+            if (!IsUnderDirectory(BaseDir))
+            {
+                return false;
+            }
+            else
+            {
+                return Names.Any(x => ContainsName(x, BaseDir.FullName.Length));
+            }
+        }
+
+        public string MakeRelativeTo(DirectoryReference Directory)
+        {
+            int CommonDirectoryLength = -1;
+            for (int Idx = 0; ; Idx++)
+            {
+                if (Idx == FullName.Length)
+                {
+                    if (Idx == Directory.FullName.Length)
+                    {
+                        return ".";
+                    }
+
+                    if (Directory.FullName[Idx] == Path.DirectorySeparatorChar)
+                    {
+                        CommonDirectoryLength = Idx;
+                    }
+                    break;
+                }
+                else if (Idx == Directory.FullName.Length)
+                {
+                    if (FullName[Idx] == Path.DirectorySeparatorChar)
+                    {
+                        CommonDirectoryLength = Idx;
+                    }
+                    break;
+                }
+                else
+                {
+                    if (String.Compare(FullName, Idx, Directory.FullName, Idx, 1, Comparison) != 0)
+                    {
+                        break;
+                    }
+                    if (FullName[Idx] == Path.DirectorySeparatorChar)
+                    {
+                        CommonDirectoryLength = Idx;
+                    }
+                }
+            }
+
+            if (CommonDirectoryLength == -1)
+            {
+                return FullName;
+            }
+
+            StringBuilder Result = new StringBuilder();
+            for (int Idx = CommonDirectoryLength + 1; Idx < Directory.FullName.Length; Idx++)
+            {
+                Result.Append("..");
+                Result.Append(Path.DirectorySeparatorChar);
+
+                while (Idx < Directory.FullName.Length && Directory.FullName[Idx] != Path.DirectorySeparatorChar)
+                {
+                    Idx++;
+                }
+            }
+            if (CommonDirectoryLength + 1 < FullName.Length)
+            {
+                Result.Append(FullName, CommonDirectoryLength + 1, FullName.Length - CommonDirectoryLength - 1);
+            }
+
+            return Result.ToString();
+        }
+
+        public string ToNormalizedPath()
+        {
+            return FullName.Replace("\\", "/");
+        }
+
+        public override string ToString()
+        {
+            return FullName;
+        }
+    }
+}
